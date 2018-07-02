8ac809dc415dbaca296a388b2169c0076d2dbeba
dtcrowley <crowleyd1988@gmail.com>
2018-07-02 13:01:32 -0700

    Added npm packages, installed materialize, created html files w/o any code. trying to get server to connect after unexpected token (import express from 'express') error

---
+++ controllers/htmlController.js
@@ -0,0 +1,20 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+router.get('/aboutus', (req, res) => {
+    res.render('aboutus');
+});
+
+router.get('/services', (req, res) => {
+    res.render('services');
+});
+
+router.get('/contactus', (req, res) => {
+    res.render('contactus');
+});
+
+export default router;
